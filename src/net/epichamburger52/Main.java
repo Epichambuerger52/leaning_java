7c469b1fd07f2394ae39d8d2dd2f521edf1f6840
jensen69 <67147309+jensen69@users.noreply.github.com>
2023-06-15 15:34:15 +0700

    compleated lesson 2

--- src/net/epichamburger52/Main.java
+++ src/net/epichamburger52/Main.java
@@ -3,6 +3,38 @@ package net.epichamburger52;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+
+        //common use
+        int anumber = 42;     //integer (whole number)
+        float afloatingnumber = 11.11f; //floating point number [7 Decimals of persision]
+        double aDoubleNumber = 420.6969; // A Double number [15 point persision]
+
+        //sometimes used
+        byte aTinyNumber = -128;  //one byte is equal to 8 bits stores numbers -128 to 127
+        short aSmallNumber = 30000; // short stores numbers from -32,768 to 32,767
+        long aLongNumber = 50000000; // longs store long ammounts of numbers
+
+        /* Booleans (Logic) */
+
+        Boolean IsAgoodBoy = true;  //Stores either True(1) or False(0)
+        Boolean IsABadBoy = true;
+
+        /* Charecters and Strings */
+
+        char ASingleCharecter ='a';     //stores a single charecter. can also include specual charecters
+
+        String message ="Hello world";  //stores a sequence of charecters
+        String test = null; // means it is blank
+
+
+
+
+
+
+
+
+
+
     }
 }
